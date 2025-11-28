9441e090c1bfbea3d3900c827fef21d69a500b9d
mathncode-sid <sidneybarakamuriuki1@gmail.com>
2025-11-28 17:24:39 +0300

    Add redirect functionality to add_item view and handle POST request

--- Admin/views.py
+++ Admin/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from Admin.models import Product
 
 # Create your views here.
 def admin(request):
     return render(request, 'admin.html')
 
 def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+
+        Product.objects.create(name=name, price=price, description=description)
+        return redirect('admin')
     return render(request, 'add_item.html')
+
